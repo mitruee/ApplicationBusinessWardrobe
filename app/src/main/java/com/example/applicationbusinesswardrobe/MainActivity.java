bb6078068c5fcb00cbaabb9b12d3d21b88bf9f4f
Dmitry <118049037+mitruee@users.noreply.github.com>
2023-02-19 14:36:22 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/applicationbusinesswardrobe/MainActivity.java
+++ app/src/main/java/com/example/applicationbusinesswardrobe/MainActivity.java
@@ -6,64 +6,56 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    // задание полей
-    float coat = 70; // пальто
-    byte coatDiscount = 77; // скидка (в процентах)
-    float hat = 25; // шляпа
-    byte hatDiscount = 37;
-    float businessCostume = 53; // деловой костюм
-    byte businessCostumeDiscount = 44;
-    float shirt = 19; // сорочка
-    float shoes = 41; // туфли
-    byte shoesDiscount = 32;
-    float accountBalance = 312; // счёт пользователя
 
-    // метод подсчёта стоимости серверного комплекта
+    float coat = 70;
+    byte coatDiscount = 77; 
+    float hat = 25; 
+    byte hatDiscount = 37;
+    float businessCostume = 53; 
+    byte businessCostumeDiscount = 44;
+    float shirt = 19; 
+    float shoes = 41; 
+    byte shoesDiscount = 32;
+    float accountBalance = 312; 
+
     private float calculation() {
-        // создание и инициализация переменной подсчёта стоимости
         float count = (coat * (100 - coatDiscount) + hat * (100 - hatDiscount)
                 + businessCostume * (100 - businessCostumeDiscount) + shirt * 100
                 + shoes * (100 - shoesDiscount)) / 100;
-        return count; // возврат подсчитанного значения
+        return count;
     }
 
-    // метод определения возможностей бюджета покупки серверного комплекта
     private boolean possibility() {
-        if (calculation() <= accountBalance) { // если стоимость комплекта меньше имеющихся средств
-            return true; // то возврат истинного значения
-        } else { // иначе
-            return false; // возврат ложного значения
+        if (calculation() <= accountBalance) {
+            return true;
+        } else {
+            return false; 
         }
     }
 
-    // метод определения возможной сдачи
     private float balance() {
-        if(possibility()) { // если имеется возможность купить товары для делового гардероба
-            return accountBalance - calculation(); // то возвращается остаток от покупки
-        } else { // иначе
-            return -1; // возвращается маркер недостатка денежных средств
+        if(possibility()) { 
+            return accountBalance - calculation(); 
+        } else { 
+            return -1; 
         }
     }
 
-    // создание дополнительных полей для вывода на экран полученных значений
-    private TextView possibilityOut; // поле возможности покупки
-    private TextView balanceOut; // поле возможного остатка от покупки
+    private TextView possibilityOut; 
+    private TextView balanceOut; 
 
-    // вывод на экран полученных значений
     @Override
-    protected void onCreate(Bundle savedInstanceState) { // создание жизненного цикла активности
+    protected void onCreate(Bundle savedInstanceState) { 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // присваивание жизненному циклу активити представления activity_main
+        setContentView(R.layout.activity_main); 
 
-        // присваивание переменным активити элементов представления activity_main
-        possibilityOut = findViewById(R.id.possibilityOut); // вывод информации о возможности покупки
-        balanceOut = findViewById(R.id.balanceOut); // вывод информации о возможном остатке от покупки
+        possibilityOut = findViewById(R.id.possibilityOut); 
+        balanceOut = findViewById(R.id.balanceOut); 
 
-        // заполнение экрана
-        if (possibility()) { // если имеется возможность товары для делового гардероба
+        if (possibility()) { 
             possibilityOut.setText("Имеется достаточно средств для покупки товаров для делового гардероба");
             balanceOut.setText("Остаток от покупки " + balance() + " кредитов");
-        } else { // иначе
+        } else {
             possibilityOut.setText("Недостаточно средств для покупки товаров для делового гардероба");
             balanceOut.setText(" - ");
         }
